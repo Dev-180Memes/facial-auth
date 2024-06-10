ca85f6c9f5bada7c47bf9ed96c163b470433521e
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2024-06-10 10:23:35 +0100

    chore: Remove unnecessary Vue configuration files and update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugin/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugin/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
